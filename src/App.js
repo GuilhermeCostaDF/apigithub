83f80e0d56738cb8077d23b88b90963f5bf29feb
GuilhermeCostaDF <guido.costa@globo.com>
2022-06-27 12:48:52 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from "./Components/layout";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <main>
+
+      <Layout >
+        <div>
+          <img src="https://avatars.githubusercontent.com/u/74997292?v=4" alt="avatar do usuario" /> 
+          <h1>Guilherme Costa</h1>
+          <h3>Username</h3>
+          <span>GuilhermeCostaDF</span>
+          <div>
+            <h4>Followers</h4>
+            <span>10</span>
+          </div>
+          <div>
+            <h4>Starreds</h4>
+            <span>10</span>
+          </div>
+          <div>
+            <h4>Following</h4>
+            <span>10</span>
+          </div>
+        </div>
+        <div>Repositories</div>
+        <div>Starred</div>
+      </Layout>
+
+    </main>
+  )
 }
 
 export default App;
