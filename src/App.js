814ebab6d78fba549191f13b8200164d3bf3e0d8
GuilhermeCostaDF <guido.costa@globo.com>
2022-06-27 12:52:36 -0300

    testando github no vscode

--- src/App.js
+++ src/App.js
@@ -7,21 +7,7 @@ function App() {
       <Layout >
         <div>
           <img src="https://avatars.githubusercontent.com/u/74997292?v=4" alt="avatar do usuario" /> 
-          <h1>Guilherme Costa</h1>
-          <h3>Username</h3>
-          <span>GuilhermeCostaDF</span>
-          <div>
-            <h4>Followers</h4>
-            <span>10</span>
-          </div>
-          <div>
-            <h4>Starred</h4>
-            <span>10</span>
-          </div>
-          <div>
-            <h4>Following</h4>
-            <span>10</span>
-          </div>
+      
         </div>
         <div>Repositories</div>
         <div>Starred</div>
